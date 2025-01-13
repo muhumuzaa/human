cb5e00fe1f9eb07d010f333f2dc99deea47490b6
muhumuzaa <muhumuzaamb@gmail.com>
2025-01-13 17:12:44 -0500

    added the sidebar component and made it sticky. Learned that when you made a component sticky in its own component but display it another component, u should also make it sticky in the parent component. The same applies to making it full width on a screen.

--- frontend/src/components/dashboard/AdminSidebar.jsx
+++ frontend/src/components/dashboard/AdminSidebar.jsx
@@ -1,6 +1,6 @@
 import {
+  FaArrowCircleLeft,
   FaHouseUser,
-  FaMoneyBill,
   FaMoneyBillAlt,
   FaWalking,
 } from "react-icons/fa";
@@ -13,40 +13,40 @@ import { NavLink } from "react-router-dom";
 
 const AdminSidebar = () => {
   return (
-    <div className="flex flex-col justify-between h-full">
+    <div className="flex flex-col justify-between min-h-screen py-8">
       <a href="#" className="font-bold">
         Human Force
       </a>
 
       <div className="mt-8">
-        <NavLink className="flex space-x-2 items-center rounded-lg text-gray-700 text-sm hover:text-white hover:bg-indigo-600 py-2 px-2 mb-2">
+        <NavLink className="flex space-x-2 items-center rounded-lg text-gray-700 text-sm hover:text-white hover:bg-indigo-600 py-3 px-2 mb-2">
           <FaHouseUser className="" />
           <span className="">Dashboard</span>
         </NavLink>
-        <NavLink className="flex space-x-2 items-center rounded-lg text-gray-700 text-sm hover:text-white hover:bg-indigo-600 py-2 px-2 mb-2">
+        <NavLink className="flex space-x-2 items-center rounded-lg text-gray-700 text-sm hover:text-white hover:bg-indigo-600 py-3 px-2 mb-2">
           <FaPeopleGroup className="" />
           <span className="">Employees</span>
         </NavLink>
-        <NavLink className="flex space-x-2 items-center rounded-lg text-gray-700 text-sm hover:text-white hover:bg-indigo-600 py-2 px-2 mb-2">
+        <NavLink className="flex space-x-2 items-center rounded-lg text-gray-700 text-sm hover:text-white hover:bg-indigo-600 py-3 px-2 mb-2">
           <FaBuilding className="" />
           <span className="">Departments</span>
         </NavLink>
-        <NavLink className="flex space-x-2 items-center rounded-lg text-gray-700 text-sm hover:text-white hover:bg-indigo-600 py-2 px-2 mb-2">
+        <NavLink className="flex space-x-2 items-center rounded-lg text-gray-700 text-sm hover:text-white hover:bg-indigo-600 py-3 px-2 mb-2">
           <FaWalking className="" />
           <span className="">Leaves</span>
         </NavLink>
-        <NavLink className="flex space-x-2 items-center rounded-lg text-gray-700 text-sm hover:text-white hover:bg-indigo-600 py-2 px-2 mb-2">
+        <NavLink className="flex space-x-2 items-center rounded-lg text-gray-700 text-sm hover:text-white hover:bg-indigo-600 py-3 px-2 mb-2">
           <FaMoneyBillAlt className="" />
           <span className="">Salary</span>
         </NavLink>
-        <NavLink className="flex space-x-2 items-center rounded-lg text-gray-700 text-sm hover:text-white hover:bg-indigo-600 py-2 px-2 mb-2">
+        <NavLink className="flex space-x-2 items-center rounded-lg text-gray-700 text-sm hover:text-white hover:bg-indigo-600 py-3 px-2 mb-2">
           <FaPhone className="" />
           <span className="">Support</span>
         </NavLink>
       </div>
       <div>
-        <NavLink className="flex space-x-2 items-center rounded-lg text-gray-700 text-sm hover:text-white hover:bg-indigo-600 py-2 px-2 mb-2">
-          <FaPhone className="" />
+        <NavLink className="flex space-x-2 items-center rounded-lg text-gray-700 text-sm hover:text-white hover:bg-indigo-600 py-3 px-2 mb-2">
+          <FaArrowCircleLeft className="" />
           <span className="">Log Out</span>
         </NavLink>
       </div>
--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -1,5 +1,6 @@
 // import { useEffect } from "react";
 // import { useNavigate } from "react-router-dom";
+//import AdminSidebar from "../components/dashboard/AdminSidebar";
 import AdminSidebar from "../components/dashboard/AdminSidebar";
 import { useAuth } from "../context/AuthContext";
 
@@ -7,14 +8,17 @@ const AdminDashboard = () => {
   const { user } = useAuth();
 
   return (
-    <div className="bg-gradient-to-b from-white to-indigo-100">
-      <div className="max-w-6xl mx-auto flex min-h-screen items-center justify-center space-x-6 pt-6">
-        <div className="w-1/4 ">
-          <AdminSidebar />
+    <div className="bg-gradient-to-b from-white to-indigo-200">
+      <div className="max-w-5xl mx-auto flex space-x-10 min-h-screen">
+
+        <div className="w-1/4 h-full sticky top-0">
+        <AdminSidebar />
         </div>
+
         <div className="w-3/4">
-          <div className="w-700px bg-red-500 h-[500px]"></div>
-          <div className="w-700px bg-blue-500 h-[500px]"></div>
+          <div className="bg-red-500 h-[500px] mb-6 w-[400px]">one</div>
+          <div className="bg-blue-500 h-[500px] mb-6"></div>
+          <div className="bg-green-500 h-[500px]"></div>
         </div>
       </div>
     </div>
